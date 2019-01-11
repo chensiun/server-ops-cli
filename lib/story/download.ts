83b03e30dcf129fd83b991fd11a65cb6d0d7b26e
liuwill <liuwei_will@qq.com>
2019-01-11 11:45:15 +0800

    finish download and save story

--- lib/story/download.ts
+++ lib/story/download.ts
@@ -1,10 +1,14 @@
 import { BaseStory } from './base';
-import { Observable, zip } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
+import { Observable, zip, of } from 'rxjs';
+import { concatMap } from 'rxjs/operators'
 import * as pageApi from '../api/page.api'
 import { logger } from '../services/logger'
 import * as reader from '../services/reader'
-import chalk from 'chalk';
+import chalk from 'chalk'
+import * as path from 'path'
+import * as fs from 'fs'
+
+const distPath = path.resolve('./dist/')
 
 export default class DownloadStory implements BaseStory {
 
@@ -34,14 +38,74 @@ export default class DownloadStory implements BaseStory {
     }
 
     return zip(
+      of(page),
       pageApi.fetchPageData(page),
       pageApi.listShelf(page),
+      pageApi.listGoods(page, 'android'),
+      pageApi.listGoods(page, 'ios'),
+      pageApi.listPresaleGoods(page, 'android'),
+      pageApi.listPresaleGoods(page, 'ios'),
+      pageApi.listCoupon(page),
+      pageApi.listFlashGoods(page),
+      pageApi.fetchShareCouponPack(page),
     )
   }
 
   save(pageData): Promise<any> {
-    console.log(pageData)
-    return Promise.resolve(1)
+    const dataFields = [
+      "page_id",
+      'PAGE_DATA',
+      'SHELF_LIST',
+      "GOODS:android",
+      "GOODS:ios",
+      "PRESALE:android",
+      "PRESALE:ios",
+      "COUPON",
+      "FLASH",
+      "SHARE",
+    ]
+    const pageId = pageData[0]
+
+    const targetData = pageData.map(item => {
+      if (typeof item === 'object') {
+        return item.data
+      }
+      return item
+    })
+    const targetMap = targetData.reduce((result, current, index) => {
+      let key = dataFields[index].toLowerCase()
+      let device = ''
+      if (key.indexOf(':') > 0) {
+        device = key.split(':')[1]
+        key = key.split(':')[1]
+      }
+
+      if (!device) {
+        result[key] = current
+      } else {
+        if (!result[key]) {
+          result[key] = {}
+        }
+        result[key][device] = current
+      }
+      return result
+    }, {})
+
+    targetMap['timstamp'] = Date.now()
+    targetMap['record_time'] = new Date().toLocaleString()
+
+    const filename = `page_${targetMap['page_id']}.json`
+    if (!fs.existsSync(distPath)) {
+      fs.mkdirSync(distPath)
+    }
+    fs.writeFileSync(path.join(distPath, `page_${targetMap['page_id']}.json`), JSON.stringify(targetMap), 'utf-8')
+    logger.info([
+      chalk.yellow('> Save File '),
+      chalk.cyan(filename),
+      "To",
+      chalk.gray(distPath),
+    ].join(' '))
+    return Promise.resolve(targetMap)
   }
 
   execute(): Observable<any> {
